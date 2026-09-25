05b8a83f377eeff971193500d0c12e52a437d9fe
NANO1903 <fernando.beristain.alvarez@gmail.com>
2026-09-24 21:01:40 -0600

    Add: two-integer-sum - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum/submission-1.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<size_t, int> umap;
+
+        for (int i = 0; i < nums.size(); ++i) 
+            umap[nums[i]] = i;
+       
+        size_t diff = 0;
+        for (int i = 0; i < nums.size(); ++i) {
+            diff = static_cast<size_t>(target) - nums[i];
+                        
+            if (umap[diff] && umap[diff] != i) return {i, umap[diff]};
+        }
+
+        return {0, 0};
+    }
+};
