6276d9a3db850b702ccb2339f686cc5c56ae1c0c
NANO1903 <fernando.beristain.alvarez@gmail.com>
2026-09-24 19:57:46 -0600

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.cpp
@@ -0,0 +1,52 @@
+/*
+
+Given two strings s and t, return true if the two strings are anagrams of each other, otherwise return false.
+
+Two strings are anagrams if they contain the same characters, with each character appearing the same number of times, regardless of order.
+
+
+Example 1:
+
+Input: s = "racecar", t = "carrace"
+
+Output: true
+
+Example 2:
+
+Input: s = "jar", t = "jam"
+
+Output: false
+
+Example 3:
+
+Input: s = "x", t = "x"
+
+Output: true
+
+Constraints:
+
+1 <= s.length, t.length <= 5 * 10^4
+s and t consist of lowercase English letters.
+
+*/
+
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        multiset<char> mSet;
+
+        if (s.length() != t.length()) return false;
+
+        for (char a : s) {
+            mSet.insert(a);
+        }
+
+        for (char a : t) {
+            if (auto iter = mSet.find(a); iter != mSet.end()) mSet.erase(iter);
+            else return false;
+        }
+
+        return true;
+    }
+};
