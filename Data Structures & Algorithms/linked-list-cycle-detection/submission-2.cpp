1c713932317127e9b0d171b59d3ccc0b3f39ec70
NANO1903 <fernando.beristain.alvarez@gmail.com>
2026-09-26 12:19:17 -0600

    Add: linked-list-cycle-detection - submission-2

---
+++ Data Structures & Algorithms/linked-list-cycle-detection/submission-2.cpp
@@ -0,0 +1,62 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+ /*
+Given the beginning of a linked list head, return true if there is a cycle in the linked list. Otherwise, return false.
+
+There is a cycle in a linked list if at least one node in the list can be visited again by following the next pointer.
+
+Internally, index determines the index of the beginning of the cycle, if it exists. The tail node of the list will set it's next pointer to the index-th node. If index = -1, then the tail node points to null and no cycle exists.
+
+Note: index is not given to you as a parameter.
+
+Example 1:
+
+Input: head = [1,2,3,4], index = 1
+
+Output: true
+Explanation: There is a cycle in the linked list, where the tail connects to the 1st node (0-indexed).
+
+Example 2:
+
+Input: head = [1,2], index = -1
+
+Output: false
+Constraints:
+
+0 <= Length of the list <= 1000.
+-1000 <= Node.val <= 1000
+index is -1 or a valid index in the linked list.
+*/
+
+class Solution {
+public:
+    bool hasCycle(ListNode* head) {
+        ListNode* fptr = head,* sptr = head;
+
+        int count = 0;
+        while (fptr && sptr) {
+            count++;
+            if (count > 2) {
+                count = 0;
+                sptr = sptr->next;
+            }
+            if (fptr->next == nullptr ||
+                sptr->next == nullptr) return false;
+            else if (fptr->next == sptr ||
+                     sptr->next == fptr &&
+                     count > 0) return true;
+                
+            fptr = fptr->next;
+        }
+        return false;
+    }
+};
